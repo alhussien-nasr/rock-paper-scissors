a4b6058a3fda37ed3b6c2c94a5e38c324101aabb
alhussien-nasr <alhussien.n.g@gmail.com>
2024-10-04 22:49:46 +0300

    add ui

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+const paper = document.querySelector("#paper");
+const rock = document.querySelector("#rock");
+const scissors = document.querySelector("#scissors");
+const div = document.querySelector("div");
+const play = document.querySelector("#play");
+let HumanChoice = "";
+let humanScore = 0;
+let computerScore = 0;
+
+console.log(paper);
+
 const getComputerChoice = () => {
   let rand = Math.floor(Math.random() * 3) + 1;
 
@@ -11,39 +22,50 @@ const getComputerChoice = () => {
   }
 };
 
-const getHumanChoice = () => {
+const getHumanChoice = (e) => {
   const HumanChoice = prompt("choise");
   console.log(HumanChoice);
   return HumanChoice;
 };
 
+paper.addEventListener("click", (e) => {
+  HumanChoice = "paper";
+  div.innerHTML = "paper";
+});
+rock.addEventListener("click", (e) => {
+  HumanChoice = "rock";
+  div.innerHTML = "rock";
+});
+scissors.addEventListener("click", (e) => {
+  HumanChoice = "scissors";
+  div.innerHTML = "scissors";
+});
+
 const playGame = () => {
-  let humanScore = 0;
-  let computerScore = 0;
+  const humanSelection = HumanChoice;
+  const computerSelection = getComputerChoice();
 
-  const playRound = () => {
-    const humanSelection = getHumanChoice().toLowerCase();
-    const computerSelection = getComputerChoice();
-
-    if (
-      (humanSelection === "paper" && computerSelection === "rock") ||
-      (humanSelection === "scissors" && computerSelection === "paper") ||
-      (humanSelection === "rock" && computerSelection === "scissors")
-    ) {
-      humanScore++;
-    } else if (
-      (computerSelection === "paper" && humanSelection === "rock") ||
-      (computerSelection === "scissors" && humanSelection === "paper") ||
-      (computerSelection === "rock" && humanSelection === "scissors")
-    ) {
-      computerScore++;
-    }
-  };
-
-  for (let i = 0; i <= 5; i++) {
-    playRound();
-    console.log(humanScore, computerScore);
+  if (
+    (humanSelection === "paper" && computerSelection === "rock") ||
+    (humanSelection === "scissors" && computerSelection === "paper") ||
+    (humanSelection === "rock" && computerSelection === "scissors")
+  ) {
+    humanScore++;
+  } else if (
+    (computerSelection === "paper" && humanSelection === "rock") ||
+    (computerSelection === "scissors" && humanSelection === "paper") ||
+    (computerSelection === "rock" && humanSelection === "scissors")
+  ) {
+    computerScore++;
+  }
+  div.innerHTML = humanScore + "  " + computerScore;
+  if (humanScore == 5) {
+    div.innerText = "you winner";
+    play.disabled = true;
+  } else if (computerScore == 5) {
+    div.innerText = "c winner";
+    play.disabled = true;
   }
 };
 
-playGame();
+play.addEventListener("click", playGame);
